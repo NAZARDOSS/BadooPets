6fde7ee6c8a30be8533c306eebbf0d6e5cf0e0e2
Nazar Dosmukhambetov <nazardosmukhambetov@m2s-macbook-air.speedport.ip>
2024-05-28 00:05:27 +0200

    ready to use, backend was updated

--- BadooDog/BadooDog/Controllers/DogsStorageController.cs
+++ BadooDog/BadooDog/Controllers/DogsStorageController.cs
@@ -1,146 +1,121 @@
 ﻿using System.Data;
 using BadooDog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MySql.Data.MySqlClient;
 
-namespace BadooDog.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class DogsStorageController: Controller
+namespace BadooDog.Controllers
 {
-    private readonly IConfiguration _configuration;
-    private readonly ILogger<DogsStorageController> _logger;
-
-    public DogsStorageController(IConfiguration configuration, ILogger<DogsStorageController> logger)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DogsStorageController : Controller
     {
-        _configuration = configuration;
-        _logger = logger;
-    }
-    
-    [HttpGet]
-    public JsonResult Get()
-    {
-        string query = @"select id, name, age, image_url, description from test.dog;";
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DogsStorageController> _logger;
 
-        DataTable table = new DataTable();
-        string sqlDataSource = _configuration.GetConnectionString("BadooDogAppCon");
-        MySqlDataReader myReader;
-        using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+        public DogsStorageController(IConfiguration configuration, ILogger<DogsStorageController> logger)
         {
-            mycon.Open();
-            using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
-            {
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-                
-                myReader.Close();
-                mycon.Close();
-            }
+            _configuration = configuration;
+            _logger = logger;
         }
-        return new JsonResult(table);
-    }
-    
-    [HttpPost]
-    public IActionResult  Post(Dog dog)
-    {
-        string query = @"
-                    insert into test.dog(id, name, age, image_url, description) values 
-                                             (@id, @name, @age, @imageURL, @description);
-        ";
 
-        try
+        [HttpGet]
+        public JsonResult Get()
         {
+            string query = @"select id, name, age, image_url, description, price, type_, gender from test.dog;";
+
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("BadooDogAppCon");
-            //MySqlDataReader myReader;
+            MySqlDataReader myReader;
             using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
             {
                 mycon.Open();
                 using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
                 {
-                    myCommand.Parameters.AddWithValue("@id", dog.Id);
-                    myCommand.Parameters.AddWithValue("@name", dog.Name);
-                    myCommand.Parameters.AddWithValue("@age", dog.Age);
-                    myCommand.Parameters.AddWithValue("@imageURL", dog.imageURL);
-                    myCommand.Parameters.AddWithValue("@description", dog.description);
-
-                    using (MySqlDataReader myReader = myCommand.ExecuteReader())
-                    {
-                        table.Load(myReader);
-                        myReader.Close();
-                    }
-                    //myReader = myCommand.ExecuteReader();
-                    mycon.Close();
-                }
-            }
-            return Ok("Added successfully");
-        }
-        catch (MySqlException e)
-        {
-            _logger.LogError(e.Message);
-            return StatusCode(500, e.Message);
-        }
-    }
-    
-    [HttpPut("id={id}")]
-    public JsonResult Put(int id, Dog dog)
-    {
-        string query = @"
-                    update test.dog
-                    set name = @name, 
-                        age = @age,
-                        image_url = @imageURL,
-                        description = @description
-                    where id = @id;
-        ";
-
-        DataTable table = new DataTable();
-        string sqlDataSource = _configuration.GetConnectionString("BadooDogAppCon");
-        using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
-        {
-            mycon.Open();
-            using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
-            {
-                myCommand.Parameters.AddWithValue("@id", id);
-                myCommand.Parameters.AddWithValue("@name", dog.Name);
-                myCommand.Parameters.AddWithValue("@age", dog.Age);
-                myCommand.Parameters.AddWithValue("@imageURL", dog.imageURL);
-                myCommand.Parameters.AddWithValue("@description", dog.description);
-
-                using (MySqlDataReader myReader = myCommand.ExecuteReader())
-                {
+                    myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
                 }
-                mycon.Close();
             }
+            return new JsonResult(table);
         }
-        return new JsonResult("Updated successfully");
-    }
-    
-    [HttpDelete("id={id}")]
-    public JsonResult Delete(int id)
+
+        [HttpPost]
+public IActionResult Post(Dog dog)
+{
+    string query = @"
+        insert into test.dog(name, age, image_url, description, price, type_, gender) 
+        values (@name, @age, @image_url, @description, @price, @type_, @gender);
+    ";
+
+    try
     {
-        string query = @"
-                    delete from test.dog
-                    where id = @id;
-        ";
-        
-        DataTable table = new DataTable();
         string sqlDataSource = _configuration.GetConnectionString("BadooDogAppCon");
         using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
         {
             mycon.Open();
             using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
             {
-                myCommand.Parameters.AddWithValue("@id", id);
+                myCommand.Parameters.AddWithValue("@name", dog.Name);
+                myCommand.Parameters.AddWithValue("@age", dog.Age);
+                // Combine all imageURLs into a single string with commas
+                string imageURLString = string.Join(",", dog.ImageURL);
+                myCommand.Parameters.AddWithValue("@image_url", imageURLString);
+                myCommand.Parameters.AddWithValue("@description", dog.Description);
+                myCommand.Parameters.AddWithValue("@price", dog.Price);
+                myCommand.Parameters.AddWithValue("@type_", dog.Type_);
+                myCommand.Parameters.AddWithValue("@gender", dog.Gender);
 
-                myCommand.ExecuteReader();
-                
+                myCommand.ExecuteNonQuery();
+            }
+            mycon.Close();
+        }
+        return Ok("Added successfully");
+    }
+    catch (MySqlException e)
+    {
+        _logger.LogError(e.Message);
+        return StatusCode(500, e.Message);
+    }
+}
+
+
+        [HttpPut("id={id}")]
+        public JsonResult Put(int id, Dog dog)
+        {
+            string query = @"
+                update test.dog
+                set name = @name, 
+                    age = @age,
+                    image_url = @image_url,
+                    description = @description,
+                    price = @price,
+                    type_ = @type_,
+                    gender = @gender
+                where id = @id;
+            ";
+
+            string sqlDataSource = _configuration.GetConnectionString("BadooDogAppCon");
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myCommand.Parameters.AddWithValue("@name", dog.Name);
+                    myCommand.Parameters.AddWithValue("@age", dog.Age);
+                    myCommand.Parameters.AddWithValue("@image_url", string.Join(",", dog.ImageURL));
+                    myCommand.Parameters.AddWithValue("@description", dog.Description);
+                    myCommand.Parameters.AddWithValue("@price", dog.Price);
+                    myCommand.Parameters.AddWithValue("@type_", dog.Type_);
+                    myCommand.Parameters.AddWithValue("@gender", dog.Gender);
+
+                    myCommand.ExecuteNonQuery();
+                }
                 mycon.Close();
             }
+            return new JsonResult("Updated successfully");
         }
-        return new JsonResult("Deleted successfully");
     }
 }
--- BadooDog/BadooDog/Models/Dog.cs
+++ BadooDog/BadooDog/Models/Dog.cs
@@ -1,14 +1,14 @@
-﻿namespace BadooDog.Models;
-
-public class Dog
+﻿namespace BadooDog.Models
 {
-    public int Id { get; set; }
-    
-    public string Name { get; set; }
-    
-    public int Age { get; set; }
-    
-    public string imageURL { get; set; }
-    
-    public string description { get; set; }
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> ImageURL { get; set; } // Change type to List<string>
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Type_ { get; set; }
+        public string Gender { get; set; }
+    }
 }
